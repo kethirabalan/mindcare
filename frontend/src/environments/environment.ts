8ce098efb2544ff00e6fbb824b14511e70b443db
kethirabalan <kethirabalantvl@gmail.com>
2025-07-18 17:22:04 +0530

    Init

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,13 +6,6 @@ export const environment = {
   production: false,
   apiUrl: 'https://mindcare-backend-wh0x.onrender.com',
   firebase: {
-    apiKey: "AIzaSyDed8T9PUlwiRJxH65WKUaGl8V3eir3JWQ",
-    authDomain: "teak-catwalk-407506.firebaseapp.com",
-    projectId: "teak-catwalk-407506",
-    storageBucket: "teak-catwalk-407506.firebasestorage.app",
-    messagingSenderId: "954470909899",
-    appId: "1:954470909899:web:25e8bd7ae2cec2ef36584e",
-    measurementId: "G-K43HSHP6VJ"
   },
 };
 
